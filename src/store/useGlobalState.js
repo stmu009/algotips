131d2ad6a56b21e9e72ec224919edf524c6a4c69
Damian Ramirez <stmu009@yahoo.com>
2021-11-18 14:04:33 -0600

    remomved unused code

--- src/store/useGlobalState.js
+++ src/store/useGlobalState.js
@@ -1,49 +1,7 @@
-import { useState, useMemo, useCallback } from "react";
-// import { makeApiRequest } from "../utils";
+import { useState, useMemo } from "react";
 
 export const useGlobalState = () => {
-  // const [recipeList, setRecipeList] = useState(null);
-  // const [reviewBarOpen, setReviewBarOpen] = useState(false);
-  // const [loading, setLoading] = useState(true);
-  // const [reviewedRecipe, setReviewedRecipe] = useState(null);
   const [topic, setTopic] = useState(null)
-
-  // const searchByName = useMemo(
-  //   () => (keyword) => {
-  //     makeApiRequest(
-  //       `/api/search-by?keyword=${keyword}`,
-  //       (data) => setRecipeList(data.meals),
-  //       setLoading
-  //     );
-  //   },
-  //   [setLoading]
-  // );
-
-  // const searchByIngredients = useMemo(
-  //   () => (ingredients) => {
-  //     makeApiRequest(
-  //       `/api/filter-by?filterType=i&filterValue=${ingredients}`,
-  //       (data) => setRecipeList(data.meals),
-  //       setLoading
-  //     );
-  //   },
-  //   [setLoading]
-  // );
-
-  // const openReviewBar = useMemo(
-  //   () => () => setReviewBarOpen(true),
-  //   [setReviewBarOpen]
-  // );
-
-  // const closeReviewBar = useMemo(
-  //   () => () => setReviewBarOpen(false),
-  //   [setReviewBarOpen]
-  // );
-  
-  // const resetReviewState = useCallback(() => {
-  //   setReviewedRecipe(null);
-  //   closeReviewBar();
-  // }, [closeReviewBar]);
 
   const setCurrentTopic = useMemo(
     (t) => (t) => setTopic(t),
--- src/utils/index.js
+++
@@ -1,2 +0,0 @@
-
-export { makeApiRequest } from "./makeApiRequest";
